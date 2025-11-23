a3b0b916104347d51302b6cda1938e9429380696
MousumiTani <tani.ecekuet@gmail.com>
2025-11-23 15:59:43 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,205 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 3000;
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.y9d5q6q.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let moviesCollection;
+
+async function run() {
+  try {
+    // await client.connect();
+    const db = client.db("movie-db");
+    moviesCollection = db.collection("movies");
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+  }
+}
+run();
+
+app.get("/", async (req, res) => {
+  try {
+    const movies = await moviesCollection.find().toArray();
+    res.send(movies);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching movies", error });
+  }
+});
+
+// GET all movies
+app.get("/movies", async (req, res) => {
+  try {
+    const { genres, minRating, maxRating } = req.query;
+    const query = {};
+
+    if (genres) query.genre = { $in: genres.split(",") };
+    if (minRating)
+      query.rating = { ...query.rating, $gte: parseFloat(minRating) };
+    if (maxRating)
+      query.rating = { ...query.rating, $lte: parseFloat(maxRating) };
+
+    const movies = await moviesCollection.find(query).toArray();
+    res.send(movies);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching all movies", error });
+  }
+});
+
+// Featured movies for slides
+app.get("/movies/featured", async (req, res) => {
+  try {
+    const movies = await moviesCollection.find().limit(5).toArray();
+    res.send(movies);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching featured movies", error });
+  }
+});
+
+// top-rated movies
+app.get("/movies/top-rated", async (req, res) => {
+  try {
+    const topRated = await moviesCollection
+      .find()
+      .sort({ rating: -1 })
+      .limit(5)
+      .toArray();
+    res.send(topRated);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching top rated movies", error });
+  }
+});
+
+//movie by ID
+app.get("/movies/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await moviesCollection.findOne({ _id: new ObjectId(id) });
+    if (!movie) return res.status(404).send({ message: "Movie not found" });
+    res.send(movie);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching movie", error });
+  }
+});
+
+// Addmovie
+app.post("/movies/add", async (req, res) => {
+  try {
+    const movieData = req.body;
+    const result = await moviesCollection.insertOne(movieData);
+    res.send({ message: "Movie added", movieId: result.insertedId });
+  } catch (error) {
+    res.status(500).send({ message: "Error adding movie", error });
+  }
+});
+
+// Updatemovie
+app.put("/movies/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    const movie = await moviesCollection.findOne({ _id: new ObjectId(id) });
+    if (!movie) return res.status(404).send({ message: "Movie not found" });
+
+    if (movie.addedBy !== updatedData.userId) {
+      return res.status(403).send({ message: "Not authorized" });
+    }
+    delete updatedData.addedBy;
+
+    await moviesCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedData }
+    );
+    res.send({ message: "Movie updated" });
+  } catch (error) {
+    res.status(500).send({ message: "Error updating movie", error });
+  }
+});
+
+// Delete movie
+app.delete("/movies/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const movie = await moviesCollection.findOne({ _id: new ObjectId(id) });
+    if (!movie) return res.status(404).send({ message: "Movie not found" });
+
+    if (movie.addedBy !== userId) {
+      return res.status(403).send({ message: "Not authorized" });
+    }
+
+    await moviesCollection.deleteOne({ _id: new ObjectId(id) });
+    res.send({ message: "Movie deleted" });
+  } catch (error) {
+    res.status(500).send({ message: "Error deleting movie", error });
+  }
+});
+
+// my collection
+app.get("/movies/my-collection", async (req, res) => {
+  try {
+    const { userId } = req.query;
+    const movies = await moviesCollection.find({ addedBy: userId }).toArray();
+    res.send(movies);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching your collection", error });
+  }
+});
+
+// user count
+app.get("/users", (req, res) => {
+  const totalUsers = Math.floor(Math.random() * 451) + 50;
+  res.send([{ totalUsers }]);
+});
+
+//watchlist
+app.patch("/movies/:id/watchlist", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userEmail } = req.body;
+
+    if (!userEmail)
+      return res.status(400).send({ message: "userEmail is required" });
+
+    const movie = await moviesCollection.findOne({ _id: new ObjectId(id) });
+    if (!movie) return res.status(404).send({ message: "Movie not found" });
+
+    const currentWatchlist = movie.watchlist || [];
+    const updatedWatchlist = currentWatchlist.includes(userEmail)
+      ? currentWatchlist.filter((email) => email !== userEmail)
+      : [...currentWatchlist, userEmail];
+
+    await moviesCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { watchlist: updatedWatchlist } }
+    );
+
+    res.send({
+      message: updatedWatchlist.includes(userEmail)
+        ? "Added to watchlist"
+        : "Removed from watchlist",
+      watchlist: updatedWatchlist,
+    });
+  } catch (err) {
+    res.status(500).send({ message: "Failed to update watchlist", error: err });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
